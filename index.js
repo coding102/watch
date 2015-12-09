9a1c787d64c6e87796e568c5e620b2512002b790
Marck <mercercountynj@gmail.com>
2015-12-09 16:54:43 -0500

    move to css

--- index.js
+++ index.js
@@ -6,53 +6,34 @@ $(document).ready(function(){
         var date = new Date();
         var hour = date.getHours();
         var minute = date.getMinutes();
+        var second = date.getSeconds();
         var millisecond = date.getMilliseconds();
         
         if (hour > 12) {
-            console.log (hour-12);
-            hour-=12;
-        }
+            hour -= 12;
+        };
+         if(hour === 0) {
+             hour = 12;
+        };
         
+        if (hour < 10) {
+            hour = ("0" + hour);
+        };
         
+        if (minute < 10) {
+            minute = ("0" + minute);
+        };
         
-        
-        
-        
+        if (second < 10) {
+            second = ("0" + second);
+        };
         
         $("#hour").text(hour);
-        $("#minute").text(date.getMinutes());
-        $("#second").text(date.getSeconds());
-        $("#millisecond").text(date.getMilliseconds());
+        $("#minute").text(minute);
+        $("#second").text(second);
+        $("#millisecond").text(millisecond);
 
-    }, 100);
+    }, 10);
     
     
     });
-//    setInterval( function() {
-//        var hours = new Date().getHours();
-//        $("#hour").html(( hours < 10 ? "0" : "" ) + hours);
-//
-//    }, 1000);
-//    
-//    
-//    
-//    setInterval( function() {
-//	       var minutes = new Date().getMinutes();
-//	       $("#minute").html(( minutes < 10 ? "0" : "" ) + minutes);
-//        
-//    },1000);
-//    
-//    
-//    setInterval( function() {
-//	       var milliseconds = new Date().getSeconds();
-//	       $("#minute").html(( minutes < 10 ? "0" : "" ) + milliseconds);
-//        
-//    },1000);
-//    
-//    setInterval( function() {
-//	       var minutes = new Date().getMinutes();
-//	       $("#minute").html(( minutes < 10 ? "0" : "" ) + minutes);
-//        
-//    },1000);
- 
-   
